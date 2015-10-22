aa7a6d01d663e1e428f40c881d9875f95741903b
Sébastien Duperron <sduperron@viadeoteam.com>
2015-10-22 11:42:03 +0200

    [feature] add bundler support for gems management

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if ! which bundle; then
+  echo "Could not find `bundle`. Run [sudo] gem install bundle."
+  exit 1
+fi
+
+if [ -e Gemfile.lock ]; then
+  rm Gemfile.lock
+fi
+
+bundle install --binstubs=.bin --path=.bundle
+bundle clean
